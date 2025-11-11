c1044f624980c5c3b8686180a86cda8d85d9ff0d
Davi <tambaradavi@gmail.com>
2025-11-11 13:59:47 -0300

    reteste

--- teste.java
+++ teste.java
@@ -1,5 +1,5 @@
 public static void main(String[] args) {
     System.out.println("Olá mundo");
-    long tempo= System.currentTimeMillis();
+    double tempo= System.currentTimeMillis()/(1000.0 *60);
     System.out.println("Olá Mundo \n"+ tempo);
 }
