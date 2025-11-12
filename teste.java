235a5a7f25ef1b35999b40d8524e3513c5a3320e
Davi <tambaradavi@gmail.com>
2025-11-12 13:14:55 -0300

    Leitura do arquivo (padrão)

--- teste.java
+++ teste.java
@@ -1,5 +1,23 @@
-public static void main(String[] args) {
-    System.out.println("Olá mundo");
-    double tempo= System.currentTimeMillis()/(1000.0 *60);
-    System.out.println("Olá Mundo \n"+ tempo);
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class teste {
+    public static void main(String[] args) {
+        // Chamando a função
+        lerArquivo("arquivos_base/nome.txt");
+    }
+    
+    // Função para ler arquivo
+    public static void lerArquivo(String caminhoArquivo) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(caminhoArquivo))) {
+            String linha;
+            while ((linha = reader.readLine()) != null) {
+                System.out.println(linha);
+            }
+        } catch (IOException e) {
+            System.err.println("Erro ao ler o arquivo: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
 }
